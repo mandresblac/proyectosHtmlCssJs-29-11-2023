39ec43fc631a294ac083e2293be26288cbd149b6
manuel blanco <mandresblac@gmail.com>
2023-11-29 18:40:07 -0500

    commit7 cronometro

--- cronometro/script.js
+++ cronometro/script.js
@@ -1,1 +1,45 @@
+const stopwatch = document.getElementById("stopwatch");
+const playPauseButton = document.getElementById("play-pause");
+const secondsSphere = document.getElementById("seconds-sphere");
 
+// Variables
+let stopwatchInterval;
+let runningTime = 0;
+
+function playPause() {
+  const isPaused = !playPauseButton.classList.contains("running");
+
+  if (isPaused) {
+    playPauseButton.classList.add("running");
+    start();
+  } else {
+    playPauseButton.classList.remove("running");
+    pause();
+  }
+}
+
+function pause() {
+  // secondsSphere.style.animationPlayState = "paused";
+  // clearInterval(stopwatchInterval);
+}
+
+function start() {
+  secondsSphere.style.animation = "rotacion 60s linear infinite";
+  let startTime = Date.now() - runningTime;
+  secondsSphere.style.animationPlayState = "running";
+
+  stopwatchInterval = setInterval(() => {
+    runningTime = Date.now() - startTime;
+    stopwatch.textContent = calculateTime(runningTime);
+  }, 1000);
+}
+
+function calculateTime(runningTime) {
+  const total_seconds = Math.floor(runningTime / 1000);
+  const total_minutes = Math.floor(total_seconds / 60);
+
+  const display_seconds = (total_seconds % 60).toString().padStart(2, "0");
+  const display_minutes = total_minutes.toString().padStart(2, "0");
+
+  return `${display_minutes}:${display_seconds}`;
+}
