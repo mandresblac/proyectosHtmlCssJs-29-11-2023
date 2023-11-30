867bfec5c6327201807151682cd0213470f424be
manuel blanco <mandresblac@gmail.com>
2023-11-30 16:29:36 -0500

    commit13 calculadora

---
+++ calculadora/js/Calculadora.js
@@ -0,0 +1,22 @@
+// Clase calculadora
+class Calculadora {
+  // Metodo sumar
+  sumar(num1, num2) {
+    return num1 + num2;
+  }
+
+  // Metodo restar
+  restar(num1, num2) {
+    return num1 - num2;
+  }
+
+  // Metodo dividir
+  dividir(num1, num2) {
+    return num1 / num2;
+  }
+
+  // Metodo multiplicar
+  multiplicar(num1, num2) {
+    return num1 * num2;
+  }
+}
