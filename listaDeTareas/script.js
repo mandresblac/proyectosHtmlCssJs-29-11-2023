68e5e78c7a6bac3599dc8119fda605e866502375
manuel blanco <mandresblac@gmail.com>
2023-12-02 21:51:01 -0500

    commit17 listaDeTareas

--- listaDeTareas/script.js
+++ listaDeTareas/script.js
@@ -3,3 +3,46 @@ const dateNumber = document.getElementById("dateNumber");
 const dateText = document.getElementById("dateText");
 const dateMonth = document.getElementById("dateMonth");
 const dateYear = document.getElementById("dateYear");
+
+// Tasks Container
+const tasksContainer = document.getElementById("tasksContainer");
+
+function setDate() {
+  const date = new Date();
+  dateNumber.textContent = date.toLocaleString("es", { day: "numeric" });
+  dateText.textContent = date.toLocaleString("es", { weekday: "long" });
+  dateMonth.textContent = date.toLocaleString("es", { month: "short" });
+  dateYear.textContent = date.toLocaleString("es", { year: "numeric" });
+}
+
+setDate();
+
+function addNewTask(event) {
+  event.preventDefault();
+  const { value } = event.target.taskText;
+  if (!value) return;
+
+  const task = document.createElement("div");
+  task.classList.add("task", "roundBorder");
+  task.addEventListener("click", changeTaskState);
+  task.textContent = value;
+  tasksContainer.prepend(task);
+  event.target.reset();
+}
+
+function changeTaskState(event) {
+  event.target.classList.toggle("done");
+}
+
+function order() {
+  const done = [];
+  const toDo = [];
+  tasksContainer.childNodes.forEach((el) => {
+    el.classList.contains("done") ? done.push(el) : toDo.push(el);
+  });
+  return [...toDo, ...done];
+}
+
+function renderOrderedTasks() {
+  order().forEach((el) => tasksContainer.appendChild(el));
+}
