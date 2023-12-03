eea2bf3848c31e34f9f686f73bd07f6aad1ba517
manuel blanco <mandresblac@gmail.com>
2023-12-02 21:13:46 -0500

    commit16 listaDeTareas

--- listaDeTareas/script.js
+++ listaDeTareas/script.js
@@ -1,1 +1,5 @@
-
+// Info date
+const dateNumber = document.getElementById("dateNumber");
+const dateText = document.getElementById("dateText");
+const dateMonth = document.getElementById("dateMonth");
+const dateYear = document.getElementById("dateYear");
